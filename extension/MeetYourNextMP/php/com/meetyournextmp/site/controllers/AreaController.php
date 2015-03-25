a9e85e5e5c98cb120dcde4d4aefebc4e8561e8f0
James Baster <james@jarofgreen.co.uk>
2015-03-25 10:57:06 +0000

    When Tweet to candidates, can chose who to

--- extension/MeetYourNextMP/php/com/meetyournextmp/site/controllers/AreaController.php
+++ extension/MeetYourNextMP/php/com/meetyournextmp/site/controllers/AreaController.php
@@ -44,29 +44,54 @@ class AreaController extends \site\controllers\AreaController {
 		$trb->setIncludeDeleted(false);
 		$trb->setArea($this->parameters['area']);
 		$trb->setLimit(200);
-		$this->parameters['humans'] = $trb->fetchAll();
-
-		$tweet = ".";
-		foreach($this->parameters['humans'] as $human) {
+		$this->parameters['humans'] = array();
+		foreach($trb->fetchAll() as $human) {
 			if ($human->getTwitter()) {
-				$tweet .= '@'. $human->getTwitter().' ';
+				$this->parameters['humans'][] = $human;
 			}
 		}
 
-		$tweet .= 'Could you add your #ge2015 events to '.
-			'https://'.$app['config']->webIndexDomain.'/area/'.$this->parameters['area']->getSlugForURL();
-
-		if ($tweet) {
-
-			$url = 'https://twitter.com/intent/tweet?text='.urlencode($tweet);
-			return $app->redirect($url);
-
-		} else {
-
-			// TODO better?
-			return $app['twig']->render('site/area/humans.html.twig', $this->parameters);
-
+		// =================================== If no Candidates
+		if (!$this->parameters['humans']) {
+			return $app['twig']->render('site/area/tweetToCandidates.noHumans.html.twig', $this->parameters);
 		}
+
+		// =================================== If Tweeting
+
+		if ($request->request->has("action") and $request->request->has("action") == 'tweet') {
+
+			$humanSlugs = $request->request->get('human');
+
+			$foundHumans = false;
+			$tweet = ".";
+
+			if (is_array($humanSlugs)) {
+				foreach ($this->parameters['humans'] as $human) {
+					if (in_array($human->getSlug(), $humanSlugs)) {
+						$tweet .= '@' . $human->getTwitter() . ' ';
+						$foundHumans = true;
+					}
+				}
+			}
+
+			if ($foundHumans) {
+				$tweet .= 'Could you add your #ge2015 events to '.
+					'https://'.$app['config']->webIndexDomain.'/area/'.$this->parameters['area']->getSlugForURL();
+
+				$url = 'https://twitter.com/intent/tweet?text='.urlencode($tweet);
+				return $app->redirect($url);
+			}
+		}
+
+
+
+		// =================================== Show Form
+
+
+		return $app['twig']->render('site/area/tweetToCandidates.html.twig', $this->parameters);
+
+
+
 
 
 
