6b0f1c2b0aebfb8a303af9033d432a11710504f3
James Baster <james@jarofgreen.co.uk>
2015-06-06 16:28:28 +0100

    Site read only

--- config.live.public.web.php
+++ config.live.public.web.php
@@ -1,2 +1,6 @@
 
+
+$CONFIG->siteReadOnly = true;
+$CONFIG->siteReadOnlyReason = 'The election is over';
+
 $CONFIG->extensions = array('AddressCodeGBOpenCodePoint','CuratedLists','Contact','Comments','MeetYourNextMP');
